37fcbb9cf18ed9019ece7fb3f188f38140108ca4
Søren Mortensen <soren@neros.dev>
2020-04-27 17:08:59 +0100

    Parse connection string from provided URL

--- Persistence/DatabaseContextFactory.cs
+++ Persistence/DatabaseContextFactory.cs
@@ -1,8 +1,10 @@
 using System;
-using System.Text;
+using System.Web;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+
+using Npgsql;
 
 using Persistence.Interfaces;
 
@@ -31,12 +33,20 @@ namespace Persistence
             }
             else
             {
-                var connectionString =
-                    Encoding.UTF8.GetString(
-                        Convert.FromBase64String(options.Value.ConnectionString));
+                var connectionString = this.ConvertConnectionString(options.Value.ConnectionString);
 
                 var builder = new DbContextOptionsBuilder()
-                    .UseNpgsql(connectionString);
+                    .UseNpgsql(
+                        connectionString);
+
+                // TODO: Once migrations have been set up, use these options.
+                /*
+                var builder = new DbContextOptionsBuilder()
+                    .UseNpgsql(
+                        connectionString,
+                        pgOptions => { pgOptions.MigrationsAssembly("Persistence.Migrations"); });
+                */
+
                 this.contextOptions = builder.Options;
             }
         }
@@ -45,6 +55,25 @@ namespace Persistence
         public DatabaseContext CreateDatabaseContext()
         {
             return new DatabaseContext(this.contextOptions);
+        }
+
+        private string ConvertConnectionString(string oldString)
+        {
+            var databaseUri = new Uri(oldString);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var queryString = HttpUtility.ParseQueryString(databaseUri.Query);
+            var isSsl = queryString["ssl"] == "true";
+
+            return new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.IsDefaultPort ? 5432 : databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = isSsl ? SslMode.Prefer : 0,
+            }.ToString();
         }
     }
 }
