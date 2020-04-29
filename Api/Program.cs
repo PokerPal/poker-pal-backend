8e085e969008c99b17cf83dad128436cc994ca46
Søren Mortensen <soren@neros.dev>
2020-04-29 14:03:11 +0100

    Set up reading from secrets.json

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -27,7 +28,9 @@ namespace Api
                     logging.AddConsole(options => options.IncludeScopes = true);
                     logging.AddDebug();
                 })
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureAppConfiguration((_, b) =>
+                    b.AddJsonFile("secrets.json", true));
         }
     }
 }
